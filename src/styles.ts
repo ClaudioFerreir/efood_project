eee7f352eb755452351e91d0e77e7583feab8255
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-01-20 19:01:48 -0300

    feat: banner

---
+++ src/styles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  salmon: '#E66767',
+  ligthYellow: '#FFEBD9'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    color: ${cores.salmon};
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
+`
