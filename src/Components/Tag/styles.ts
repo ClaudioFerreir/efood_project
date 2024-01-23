8e09f915a043e0a41864ebccd6e0309eb41d5315
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-01-23 15:55:49 -0300

    feat: Tag component

---
+++ src/Components/Tag/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const TagContainer = styled.div`
+  display: inline-block;
+  background-color: ${cores.salmon};
+  color: ${cores.ligthYellow};
+  font-weight: bold;
+  border: none;
+  padding: 6px 4px;
+`
