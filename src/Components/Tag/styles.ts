e94e8781ca13275125abe2115d3852828169b8b8
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-01-23 19:05:53 -0300

    feat: button component

--- src/Components/Tag/styles.ts
+++ src/Components/Tag/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
 import { cores } from '../../styles'
 
 export const TagContainer = styled.div`
@@ -6,6 +8,18 @@ export const TagContainer = styled.div`
   background-color: ${cores.salmon};
   color: ${cores.ligthYellow};
   font-weight: bold;
+  font-size: 14px;
   border: none;
   padding: 6px 4px;
+`
+
+export const TagLink = styled(Link)`
+  display: inline-block;
+  background-color: ${cores.salmon};
+  color: ${cores.ligthYellow};
+  font-weight: bold;
+  font-size: 14px;
+  border: none;
+  padding: 4px 6px;
+  text-decoration: none;
 `
