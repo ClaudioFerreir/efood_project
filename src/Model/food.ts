01e380aacfde1e72461c78691f22bf2b5a798966
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-02-16 19:06:41 -0300

    model Food added

---
+++ src/Model/food.ts
@@ -0,0 +1,23 @@
+class Food {
+  id: number
+  image: string
+  title: string
+  description: string
+  price: number
+
+  constructor(
+    id: number,
+    image: string,
+    title: string,
+    description: string,
+    price: number
+  ) {
+    this.id = id
+    this.image = image
+    this.title = title
+    this.description = description
+    this.price = price
+  }
+}
+
+export default Food
