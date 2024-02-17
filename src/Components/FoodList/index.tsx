f8c68aa7e2830548b288f01a7b1c10960ce7a978
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-02-17 12:23:26 -0300

    chore: mock updated

--- src/Components/FoodList/index.tsx
+++ src/Components/FoodList/index.tsx
@@ -41,6 +41,7 @@ const mock: Food[] = [
         de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma
         pizza simples, mas deliciosa, que agrada a todos os paladares e é uma
         ótima opção para qualquer ocasião. <br />
+        <br />
         Serve: de 2 a 3 pessoas
       </p>
     ),
@@ -49,20 +50,18 @@ const mock: Food[] = [
   {
     id: 2,
     image: pizza,
-    title: 'Pizza Marguerita',
+    title: 'Pizza Pepperoni',
     description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      'Uma pizza clássica coberta com generosas fatias de pepperoni, queijo derretido e molho de tomate temperado.',
     details: (
       <p>
-        A pizza Margherita é uma pizza clássica da culinária italiana,
-        reconhecida por sua simplicidade e sabor inigualável. Ela é feita com
-        uma base de massa fina e crocante, coberta com molho de tomate fresco,
-        queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva
-        extra-virgem. A combinação de sabores é perfeita, com o molho de tomate
-        suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas
-        de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma
-        pizza simples, mas deliciosa, que agrada a todos os paladares e é uma
-        ótima opção para qualquer ocasião. <br />
+        A combinação irresistível de sabores defumados do pepperoni, o queijo
+        derretido e o molho de tomate picante tornam esta pizza uma escolha
+        popular para os amantes de carne. Cada mordida é uma explosão de sabor
+        que cativa os sentidos. Esta pizza é perfeita para compartilhar com
+        amigos e familiares em qualquer ocasião. Sirva com uma bebida
+        refrescante para uma refeição satisfatória e reconfortante. <br />
+        <br />
         Serve: de 2 a 3 pessoas
       </p>
     ),
@@ -71,20 +70,18 @@ const mock: Food[] = [
   {
     id: 3,
     image: pizza,
-    title: 'Pizza Marguerita',
+    title: 'Pizza Quatro Queijos',
     description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      'Uma pizza indulgente coberta com uma mistura de quatro queijos: mussarela, parmesão, gorgonzola e provolone.',
     details: (
       <p>
-        A pizza Margherita é uma pizza clássica da culinária italiana,
-        reconhecida por sua simplicidade e sabor inigualável. Ela é feita com
-        uma base de massa fina e crocante, coberta com molho de tomate fresco,
-        queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva
-        extra-virgem. A combinação de sabores é perfeita, com o molho de tomate
-        suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas
-        de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma
-        pizza simples, mas deliciosa, que agrada a todos os paladares e é uma
-        ótima opção para qualquer ocasião. <br />
+        Cada mordida desta pizza oferece uma explosão de sabores diferentes,
+        desde o cremoso até o picante. A combinação de queijos de alta qualidade
+        derretidos sobre a massa crocante é simplesmente divina. Esta pizza é um
+        deleite para os amantes de queijo e uma escolha sofisticada para uma
+        noite especial. Sirva com um bom vinho tinto para aprimorar a
+        experiência gastronômica. <br />
+        <br />
         Serve: de 2 a 3 pessoas
       </p>
     ),
@@ -93,20 +90,19 @@ const mock: Food[] = [
   {
     id: 4,
     image: pizza,
-    title: 'Pizza Marguerita',
+    title: 'Pizza Vegetariana',
     description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      'Uma pizza saudável e colorida, coberta com uma variedade de vegetais frescos, como pimentão, cebola, cogumelos e azeitonas.',
     details: (
       <p>
-        A pizza Margherita é uma pizza clássica da culinária italiana,
-        reconhecida por sua simplicidade e sabor inigualável. Ela é feita com
-        uma base de massa fina e crocante, coberta com molho de tomate fresco,
-        queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva
-        extra-virgem. A combinação de sabores é perfeita, com o molho de tomate
-        suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas
-        de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma
-        pizza simples, mas deliciosa, que agrada a todos os paladares e é uma
-        ótima opção para qualquer ocasião. <br />
+        Esta pizza é uma explosão de cores e sabores naturais. Os vegetais
+        frescos adicionam uma textura crocante e um sabor vibrante que agrada
+        até mesmo os não vegetarianos. Cada ingrediente é cuidadosamente
+        selecionado para garantir uma mistura harmoniosa de sabores e uma
+        experiência gastronômica satisfatória. Esta pizza é uma escolha
+        nutritiva e deliciosa para uma refeição leve e saudável. Sirva com uma
+        salada fresca para um banquete equilibrado. <br />
+        <br />
         Serve: de 2 a 3 pessoas
       </p>
     ),
@@ -115,20 +111,19 @@ const mock: Food[] = [
   {
     id: 5,
     image: pizza,
-    title: 'Pizza Marguerita',
+    title: 'Pizza Frango com Catupiry',
     description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      'Uma pizza brasileira clássica com frango desfiado, catupiry cremoso e uma pitada de orégano.',
     details: (
       <p>
-        A pizza Margherita é uma pizza clássica da culinária italiana,
-        reconhecida por sua simplicidade e sabor inigualável. Ela é feita com
-        uma base de massa fina e crocante, coberta com molho de tomate fresco,
-        queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva
-        extra-virgem. A combinação de sabores é perfeita, com o molho de tomate
-        suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas
-        de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma
-        pizza simples, mas deliciosa, que agrada a todos os paladares e é uma
-        ótima opção para qualquer ocasião. <br />
+        Esta pizza combina a suculência do frango desfiado com a cremosidade do
+        catupiry, resultando em uma experiência de sabor única e deliciosa. Cada
+        mordida é uma indulgência cremosa e reconfortante que agrada a todos os
+        paladares. A adição de uma pitada de orégano acrescenta um toque de
+        frescor e aroma. Esta pizza é uma escolha popular em todo o Brasil e é
+        perfeita para qualquer refeição. Sirva com um suco natural para uma
+        combinação perfeita. <br />
+        <br />
         Serve: de 2 a 3 pessoas
       </p>
     ),
@@ -137,20 +132,19 @@ const mock: Food[] = [
   {
     id: 6,
     image: pizza,
-    title: 'Pizza Marguerita',
+    title: 'Pizza Calabresa',
     description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      'Uma pizza tradicionalmente italiana com fatias de linguiça calabresa, cebola, pimentão e queijo mussarela.',
     details: (
       <p>
-        A pizza Margherita é uma pizza clássica da culinária italiana,
-        reconhecida por sua simplicidade e sabor inigualável. Ela é feita com
-        uma base de massa fina e crocante, coberta com molho de tomate fresco,
-        queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva
-        extra-virgem. A combinação de sabores é perfeita, com o molho de tomate
-        suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas
-        de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma
-        pizza simples, mas deliciosa, que agrada a todos os paladares e é uma
-        ótima opção para qualquer ocasião. <br />
+        A linguiça calabresa defumada é o destaque desta pizza, oferecendo um
+        sabor rico e levemente picante. Combinado com os vegetais frescos e o
+        queijo derretido, é uma opção clássica e satisfatória. Cada mordida é
+        uma explosão de sabores que transporta os sentidos para as ruas de
+        Nápoles. Esta pizza é perfeita para uma refeição reconfortante em
+        família ou para compartilhar com amigos. Sirva com uma cerveja gelada
+        para uma experiência autêntica. <br />
+        <br />
         Serve: de 2 a 3 pessoas
       </p>
     ),
